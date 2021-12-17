27f67b743bfe7ed6128977e87b84bc8980f45a34
Ramon Tavares <95833408+tavaresRamon@users.noreply.github.com>
2021-12-17 15:02:40 -0300

    Update MinimosQUadrados.java

--- src/GitHub1/MinimosQUadrados.java
+++ src/GitHub1/MinimosQUadrados.java
@@ -5,16 +5,17 @@ import java.util.List;
 
 public class MinimosQUadrados {
     public static void main(String[] args) {
+        //VALORES DE X E Y UTILIZADOS COMO EXEMPLO:
         List<Double> varx = new ArrayList<>(List.of(907.0, 926.0, 506.0, 741.0, 789.0, 889.0, 874.0, 510.0, 529.0, 420.0));
         List<Double> vary = new ArrayList<>(List.of(11.2, 11.05, 6.84, 9.21, 9.42, 10.08, 9.45, 6.73, 7.24, 6.12));
         double somadeX = varx.stream().mapToDouble(Double::doubleValue).sum();
-        System.out.printf("soma de x [%.3f]%n", somadeX);
+        System.out.printf("Soma de x [%.3f]%n", somadeX);
         double somadeY = vary.stream().mapToDouble(Double::doubleValue).sum();
-        System.out.printf("soma de y [%.3f]%n", somadeY);
+        System.out.printf("Soma de y [%.3f]%n", somadeY);
         double mediaX = somadeX / varx.size();
-        System.out.printf("medias de x [%.3f]%n", mediaX);
+        System.out.printf("Médias de x [%.3f]%n", mediaX);
         double mediaY = somadeY / vary.size();
-        System.out.printf("medias de Y [%.3f]%n", mediaY);
+        System.out.printf("Médias de Y [%.3f]%n", mediaY);
 
         ArrayList<Double> subDex = new ArrayList<>();
         for (Double aDouble : varx) {
@@ -27,8 +28,8 @@ public class MinimosQUadrados {
         double subDeXi = subDex.stream().filter(n -> n > 0 ? n.isNaN() : n == 0).mapToDouble(Double::doubleValue).sum();
         double mediaDeXi = subDeXi / subDex.size();
         System.out.println("-----------------------------------------------------------------------------------------------------");
-        System.out.printf("soma  de (xi - media): [%.2f]%n", subDeXi);
-        System.out.printf("media 0 de (xi - media): [%.2f]%n", mediaDeXi);
+        System.out.printf("Soma  de (xi - media): [%.2f]%n", subDeXi);
+        System.out.printf("Média 0 de (xi - media): [%.2f]%n", mediaDeXi);
         System.out.println("-----------------------------------------------------------------------------------------------------");
 
 
@@ -39,13 +40,13 @@ public class MinimosQUadrados {
         }
         for (Double aDouble : xAoQuadrado) {
 
-            System.out.printf("Valores totais de (xi - media)²: [%.3f]%n ", aDouble);
+            System.out.printf("Todos os valores de (xi - media)²: [%.3f]%n ", aDouble);
         }
         double somaXAoQuadrado = xAoQuadrado.stream().mapToDouble(Double::doubleValue).sum();
         double mediaXAoQuadrado = somaXAoQuadrado / xAoQuadrado.size();
         System.out.println("-----------------------------------------------------------------------------------------------------");
         System.out.printf("Soma total dos valores de (x-media)²: [%.3f]%n", somaXAoQuadrado);
-        System.out.printf("Media dos valores de (x-media)²: [%.3f]%n", mediaXAoQuadrado);
+        System.out.printf("Média dos valores de (x-media)²: [%.3f]%n", mediaXAoQuadrado);
         System.out.println("-----------------------------------------------------------------------------------------------------");
 
 
@@ -61,8 +62,8 @@ public class MinimosQUadrados {
         double somaYzero = subDey.stream().filter(n -> n > 0 ? n.isNaN() : n == 0).mapToDouble(Double::doubleValue).sum();
         double mediaYzero = somaYzero / subDey.size();
         System.out.println("-----------------------------------------------------------------------------------------------------");
-        System.out.printf("soma da dubtração de y com a média: [%.4f]%n", somaYzero);
-        System.out.printf("media  da subtração de y com a média: [%.4f]%n", mediaYzero);
+        System.out.printf("Soma da subtração de y com a média: [%.4f]%n", somaYzero);
+        System.out.printf("Média  da subtração de y com a média: [%.4f]%n", mediaYzero);
         System.out.println("-----------------------------------------------------------------------------------------------------");
 
 
@@ -79,7 +80,7 @@ public class MinimosQUadrados {
         double mediaXY = somaXY / produtoSubXeY.size();
         System.out.println("-----------------------------------------------------------------------------------------------------");
         System.out.printf("Soma total do produto de (xi-media)(yi-media): [%.3f]%n", somaXY);
-        System.out.printf("Media do somatorio do produto de (xi-media(yi-media): [%.3f]%n", mediaXY);
+        System.out.printf("Média do somatório do produto de (xi-media(yi-media): [%.3f]%n", mediaXY);
         System.out.println("-----------------------------------------------------------------------------------------------------");
 
 
